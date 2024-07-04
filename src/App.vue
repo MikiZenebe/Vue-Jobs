25c12dc2b7b99cdcfbe8c3278f635cc0051733a8
MikiZenebe <mickyaszenebe@gmail.com>
2024-07-03 20:52:08 -0500

    nav-hero -ui

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,19 @@
 <script script>
 import Navbar from "./components/Navbar.vue";
+import Hero from './components/Hero.vue'
+
+
+export default {
+  components: {
+    Navbar,
+    Hero
+  },
+};
 </script>
 
 <template>
-  <Navbar />
+  <div class="mx-auto">
+    <Navbar />
+    <Hero />
+  </div>
 </template>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,20 @@
 <script setup>
-// import logo from "@/assets/logo.png";
+import logo from "@/assets/logo.png";
 </script>
 
 <template>
-  <p class="text-white text-2xl">Navbar</p>
+  <nav class="bg-gradient-to-r from-green-700 to-green-600  py-5">
+    <div class="flex items-center justify-between mx-10 md:mx-20">
+      <div class="flex items-center gap-3">
+        <img class="h-10 w-auto" :src="logo" alt="Vue Jobs" />
+        <p class="text-xl font-bold">LogoName</p>
+      </div>
+
+      <div class="flex items-center gap-5">
+        <p>Home</p>
+        <p>Jobs</p>
+        <p>Add Job</p>
+      </div>
+    </div>
+  </nav>
 </template>
